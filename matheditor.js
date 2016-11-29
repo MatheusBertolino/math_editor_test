45b36d76a72a0644671d9b312c4b359153233967
Homero Barrocas Soares Esmeraldo <homerobse@gmail.com>
2016-11-29 20:29:20 -0200

    Initial blinking cursor. Still has bugs that need to be fixed

--- matheditor.js
+++ matheditor.js
@@ -1,14 +1,33 @@
 window.onload = function (){
 	var hiddentextarea = document.getElementById('hiddentextarea');
 	var renderplaceholder = document.getElementById('renderplaceholder');
+	var cursor = document.getElementById("cursor");
+	var refreshIntervalId;
 	renderButtons();
 	customizeAndLoadQuillEditor();
+
+	hiddentextarea.onfocus = function(){
+		console.log('focus');
+		refreshIntervalId = setInterval(blinkCursor, 500);
+	};
+	hiddentextarea.onblur = function(){
+		cursor.style.display = 'none';
+		console.log('blur');
+		clearInterval(refreshIntervalId);
+	};
+}
+	
+function blinkCursor(){
+	if (cursor.style.display == 'none'){
+		cursor.style.display = 'inline';}
+	else{
+		cursor.style.display = 'none';}
 }
 
 function renderButtons(){
 	var buttons = document.querySelectorAll(".math_edt_btn");
 	for (var i=0; i<buttons.length; i++){
-		katex.render(buttons[i].textContent, buttons[i]);	
+		katex.render(buttons[i].textContent, buttons[i]);
 	}
 }
 
